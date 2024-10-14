763a251cf3657b952f0b9cd77b13002c4c21aca0
akmmp241 <akmalmp241@gmail.com>
2024-10-14 16:23:46 +0700

    feat: setup routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+    Route::prefix("account")->name("account.")->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::get('/change-password', [ProfileController::class, 'password'])->name('profile.password');
+        Route::patch('/change-password', [ProfileController::class, 'changePassword'])->name('profile.change-password');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+});
+
+require __DIR__.'/auth.php';
+require __DIR__.'/resources.php';
+require __DIR__.'/master.php';
